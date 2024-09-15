4986ab5f5a6ad7a526e24323d2e50e5073ecf99c
Tc <100209049+Tc554@users.noreply.github.com>
2024-09-15 21:17:17 +0300

    Update loader.bat

--- loader.bat
+++ loader.bat
@@ -2,7 +2,7 @@
 setlocal
 
 set REPO_DIR=repo-dir
-set GITHUB_REPO=https://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@github.com/Tc554/loader-releases
+set GITHUB_REPO=https://TOKEN@github.com/Tc554/loader-releases
 set FINAL_NAME=%1
 set JAR_FILE=target\%FINAL_NAME%.jar
 
